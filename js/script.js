5c89bde70d060be42d6fdd6b50e6ba330d0aecca
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-11-20 17:06:08 -0500

    ✨ Add photo carousel; handle restart; allow photo download after photoshoot complete

--- js/script.js
+++ js/script.js
@@ -1,45 +1,101 @@
 const video = document.querySelector("video");
-			const canvas = document.querySelector("canvas");
-			const button = document.querySelector("button");
-			const ctx = canvas.getContext("2d");
-			let images = [];
-			const videoScale = 0.8;
-			const MAX_PICTURES = 100;
-			const picturesPerSecond = 10;
+const videoWrapper = document.getElementById("video-wrapper");
+const canvas = document.querySelector("canvas");
+const button = document.querySelector("button");
+const ctx = canvas.getContext("2d");
 
-			const stream = await navigator.mediaDevices.getUserMedia({
-				video: true,
-				audio: false,
-			});
+let imagesWrapper = document.createElement("div");
+let imagesCarousel = document.getElementById("images-carousel");
+imagesWrapper.classList.add("images-wrapper");
+document.body.appendChild(imagesWrapper);
 
-			video.srcObject = stream;
+let imageElements = [];
+const videoScale = 0.8;
+const MAX_PICTURES = 50;
+const picturesPerSecond = 10;
 
-			function takePicture() {
-				[canvas.width, canvas.height] = [video.videoWidth * videoScale, video.videoHeight * videoScale];
-				ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-				const image = document.createElement("img");
-				image.src = canvas.toDataURL("image/jpg");
-				image.style = `
-         --rotation: ${Math.random() * 360}deg;
-         --scale: ${Math.random() * 0.75 + 0.45};
-         --x: ${Math.random() * innerWidth}px;
-         --y: ${Math.random() * innerHeight}px;`;
+const mediaConfig = {
+	video: true,
+	audio: false,
+};
 
-				image.onload = () => {
-					images.push(image);
+let stream = await navigator.mediaDevices.getUserMedia(mediaConfig);
+video.srcObject = stream;
 
-					if (images.length > MAX_PICTURES) {
-						clearInterval(interval);
-						stream.getTracks().forEach(function (track) {
-							track.stop();
-						});
-						button.style.display = "none";
-						button.style.pointerEvents = "none";
-					} else {
-						document.body.appendChild(image);
-					}
-				};
-			}
+function takePicture() {
+	[canvas.width, canvas.height] = [video.videoWidth * videoScale, video.videoHeight * videoScale];
+	ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-			button.addEventListener("click", takePicture);
-			const interval = setInterval(takePicture, 1000 / picturesPerSecond);
+	if (imageElements.length >= MAX_PICTURES) {
+		// Stop taking photos
+		clearInterval(interval);
+
+		// Remove camera stream
+		stream.getTracks().forEach(function (track) {
+			track.stop();
+    });
+    
+    videoWrapper.style.background = `url(${imageElements[imageElements.length - 1].src})`;
+    videoWrapper.style.backgroundRepeat = "no-repeat";
+    videoWrapper.style.backgroundSize = "cover";
+    imagesCarousel.style.display = "flex";
+    video.style.opacity = 0;
+		message.innerText = "Click or tap to snap again!";
+
+		setupRestartListener();
+
+		return;
+	}
+
+	const image = document.createElement("img");
+	image.src = canvas.toDataURL("image/jpg");
+	image.style = `
+    --rotation: ${Math.random() * 60 - 60}deg;
+    --scale: ${Math.random() * 0.75 + 0.25};
+    --x: ${Math.random() * innerWidth}px;
+    --y: ${Math.random() * innerHeight}px;`;
+
+	image.onload = () => {
+    imageElements.push(image);
+    imagesWrapper.appendChild(image);
+    const copy = image.cloneNode();
+    const imageLink = document.createElement("a");
+    imageLink.download = `snapbooth_photo_${Date.now() + "_" + imageElements.length}.jpg`;
+    imageLink.href = copy.src;
+    copy.style = undefined;
+    imageLink.appendChild(copy);
+    imagesCarousel.appendChild(imageLink);
+	};
+}
+
+// button.addEventListener("click", takePicture);
+let interval = setInterval(takePicture, 1000 / picturesPerSecond);
+
+function setupRestartListener() {
+  // button.removeEventListener("click", takePicture);
+	button.addEventListener("click", async function restartCamera() {
+		stream = await navigator.mediaDevices.getUserMedia(mediaConfig);
+		video.srcObject = stream;
+
+		interval = setInterval(takePicture, 1000 / picturesPerSecond);
+
+    video.style.opacity = 1;
+		message.innerText = "Smile!";
+
+		imagesWrapper.remove();
+    imageElements = [];
+    
+		imagesWrapper = document.createElement("div");
+		imagesWrapper.classList.add("images-wrapper");
+    document.body.appendChild(imagesWrapper);
+    
+    imagesCarousel.remove();
+		imagesCarousel = document.createElement("div");
+		imagesCarousel.id = "images-carousel";
+    document.body.appendChild(imagesCarousel);
+    
+    videoWrapper.style.background = "none";
+
+		removeEventListener("click", restartCamera);
+	});
+}
